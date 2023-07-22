b6a968de6537b98c1c62cba147071c21232fdd9a
kiranthedevelopr <kirannasim05@gmail.com>
2023-07-22 04:21:21 -0700

    21-25

---
+++ app.js
@@ -0,0 +1,222 @@
+// ********* Assignment Javascript 21-25 **********
+// Question # 01
+// Q: Write a program that takes two user input for first and merge them in a new variable titled fullName.
+// Greet   the user  using his full name.
+
+// let first_name = prompt("enter ur first name") 
+// let last_name = prompt("enter ur last name") 
+
+// let full_name =  first_name+""+last_name;
+// alert ("welcome"+full_name);
+
+// Question # 02
+// Q:write a progaramme to take a user input about his favoraite
+// mobile model .Find and display the lenght of user input in your browser
+
+// let model = prompt("enter your favoraite mobile phone model")
+// let len = model.length;
+// alert("your favoraite mobile phone is: "+ model+"\nlenght of your input is: "+len);
+
+// Question # 03
+// Q: write a programme to find the index of letter  "n" in the world
+// pakistani and display the result in your browser.
+
+// let country_name= "pakistani";
+// let indexx= 0;
+// for (let i= 0;
+//    i<country_name.length;i++){
+    // if(country_name[i]== "n"){
+    // indexx = i;
+    // break;
+    // }
+// }
+
+    // document.write("index of n is : " + indexx)
+
+// Question # 04
+// Q:write a programme tov find the last index of letter "l" in the word "Hellow World" and display
+// the result in your browser.
+
+// let value = "hellow world";
+// let count = 0;
+// let index = 0;
+// for (let i = 0;
+// i<value.length;i++){
+// if (value[i]=="l"){
+    // count+=1;
+    // index = i;
+// }
+// } 
+// document.write ("frequency of l : " + count +" and last index of l : "+index)
+
+// Question # 05
+// Q:write a programme to find the character at 3rd index in the word "pakistani" and 
+// display the result in your browser.
+
+// let value ="pakistani";
+// let index = value [3];
+// document.write("character at 3rd index is : " + index);
+
+
+// Question # 06
+
+// let first_name = prompt("enter ur first name") 
+// let last_name = prompt("enter ur last name")
+// let full_name =  first_name.concat(last_name);
+// alert ("welcome"+full_name);
+
+// Question # 07
+//let city ="karachi";
+//console.log(
+//city.replace("kiran"Naseem"));
+
+
+// Question # 08
+//let message = "Ali and Sami are best friends. They Play Cricket And Football together.";
+
+//for (let i=0
+  //  ;i<message.length ; i ++){
+      //  if (message.slice(i,i+3)=== "and"){
+
+      // message=  message.replace(message.slice(i,i+3),"&");
+   // }
+//}
+   // console.log(message);
+
+ // Question # 09
+ ///let Str ="247";
+ //console.log(Str);
+ //console.log(typeof(Str));
+ //let num = +Str;
+ //console.log(num);
+// console.log(typeof(num));
+
+   // Question # 10
+  // let input = prompt("enter a string");
+  // document.write (input.toUpperCase());
+
+
+
+   // Question # 11
+   //let input =prompt("enter a srting");
+   //let first_char =
+  // (input[0]);
+   //first_char =
+   //first_char.toLocaleUpperCase();
+   //input = (input.slice(1));
+   //input =
+   //input.toLocaleLowerCase();
+
+   //let output = first_char + input
+//console.log (output);
+
+
+   // Question # 12
+   //let num = 35.36;
+   //let str = num.toString();
+  // str = str.replace(".",
+  // "");
+//console.log(str);
+
+
+   // Question # 13
+   //let input = prompt("enter a string");
+
+  // ascii codes
+//let ascii_of_exc_mark=33;
+//let ascii_of_comma=44;
+//let ascii_of_dot=46;
+//let ascii_of_At=64;
+
+//let flag = false;
+
+
+
+ //for(let i = 0 ;
+ //i<input.length;i++){
+   // let convert =
+    //input.charCodeAt(i);
+    //if (convert ===
+       // ascii_of_exc_mark || convert
+        //=== ascii_of_comma || convert
+        //=== ascii_of_dot || convert
+        //===ascii_of_At){
+            //flag = true ;
+        //}
+// }
+
+ //if(flag == true){
+      //alert ("enter valid input")
+//}
+
+//else{
+
+ // alert ("thanks for entering valid input")
+//}
+
+
+   // Question # 14
+   //let A = ["cake","applepie, "cookie","chips","patties"];
+   //let input = prompt("enter what u want");
+  // input =
+   //input.toLowerCase();
+
+  // let flag = false;
+   //for( let i=0;i<A.length;i++){
+   // if (input===A[i]){
+       // flag = true;
+    
+   // }
+   //}
+  // if(flag ==true){
+  //  console.log("available");
+  // }
+//else{
+    //console.log("unavailable");
+//}
+
+   // Question # 15
+
+   // Question # 16
+  // let university =
+  // "University of karachi";
+  // let spl;
+
+  // for (let i = 0);
+  // i<university.length ; i ++){
+   // spl =
+   // university[i].splite("");
+   // spl = spl.toSrting();
+   // if(spl===","){
+      //  spl= " ";
+   // }
+   // console.log(spl);
+   //}
+
+      // Question # 17
+      //let input = prompt("enter a string");
+      //let last_char =
+     // input[input.length-1];
+     // console.log("last character is : " + last_char);
+
+           // Question # 18
+        //let string ="The quick brown fox jumps over the lazy dog";
+           //string =
+           //string.toLowerCase();
+           //let count = 0 ;
+           //for (let i = 0;
+           //i<string.length; i ++){
+           // if(string.slice(i,i+3)
+            //=== "the"){
+               // count += 1;
+                //console.log (string.slice(i,i+3));
+            ///}
+          // }
+        //if(count >= 1){
+           // console.log("the word" 'the' has occured : "+ count +" times in given string");
+        //}
+
+
+
+
+
